4b3a80f64ee3cc70c67e1d0062f727487b31dc3a
flutterWithChris <94571307+flutterWithChris@users.noreply.github.com>
2023-08-20 10:42:55 -0700

    Pods Install

---
+++ ios/Pods/Target Support Files/PhoneNumberKit-framework/PhoneNumberKit-framework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PhoneNumberKit_framework : NSObject
+@end
+@implementation PodsDummy_PhoneNumberKit_framework
+@end
---
+++ ios/Pods/Target Support Files/PhoneNumberKit-library/PhoneNumberKit-library-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PhoneNumberKit_library : NSObject
+@end
+@implementation PodsDummy_PhoneNumberKit_library
+@end
